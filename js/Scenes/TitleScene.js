a93a17214867ad2a6477c85ca1059a9bc6c7a490
Sylvebois <thomasghaye@hotmail.com>
2019-06-27 15:42:44 +0200

    Placing elements on home screen

--- js/Scenes/TitleScene.js
+++ js/Scenes/TitleScene.js
@@ -8,14 +8,27 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create () {
+    //Title
+    let title = this.add.text(0, 0, 'Run Plombo ! Run !', { fontSize: '64px', fill: '#fff' });
+    title.setPosition(config.width / 2 - title.width / 2, title.height);
+
     // Game
-    this.gameButton = new Button(this, config.width / 2, config.height / 2 - 100, 'blueButton1', 'blueButton2', 'Play', 'Game');
+    this.gameButton = new Button(this, config.width / 2, config.height / 2 - 50, 'blueButton1', 'blueButton2', 'Play', 'Game');
 
     // Options
-    this.optionsButton = new Button(this, config.width / 2, config.height / 2, 'blueButton1', 'blueButton2', 'Options', 'Options');
+    this.optionsButton = new Button(this, config.width / 2, config.height / 2 + 50, 'blueButton1', 'blueButton2', 'Options', 'Options');
 
     // Credits
-    this.creditsButton = new Button(this, config.width / 2, config.height / 2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
+    this.creditsButton = new Button(this, config.width / 2, config.height / 2 + 150, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
+
+    //Bestscore
+    let bestScoreText = this.add.text(0, 0, `Best : ${this.sys.game.globals.bestScore}`, { fontSize: '32px', fill: '#fff' });
+    bestScoreText.setPosition(config.width / 2 - bestScoreText.width / 2, config.height / 2 + 250);
+
+    // Plombo
+    let plomboFace = this.add.image(0, 0, (this.sys.game.globals.bonus)? 'plombo_unlocked' : 'plombo');
+    plomboFace.setPosition(this.gameButton.x, this.gameButton.y - plomboFace.height / 2);
+    plomboFace.scale = 0.5;
   }
 
   centerButton (gameObject, offset = 0) {
